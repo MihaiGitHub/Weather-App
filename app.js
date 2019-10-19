4df5e6e54e06e8b335b435cb48f939a946e88830
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-19 16:28:49 -0700

    Create http request and parse json response

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request')
+
+const url = 'xxxx'
+
+// Parse request as json
+request({ url: url, json: true }, (error, response) => {
+    console.log('It is currently ', response.body.currently.temperature)
+})
