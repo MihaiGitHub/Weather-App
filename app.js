b9a50f7432e0a88c578aee9901f7c8b2058f2c90
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-19 17:44:11 -0700

    Error handling in http requests

--- app.js
+++ app.js
@@ -4,5 +4,29 @@ const url = 'xxxx'
 
 // Parse request as json
 request({ url: url, json: true }, (error, response) => {
-    console.log('It is currently ', response.body.currently.temperature)
+    // Fail at a low level; error argument exists, but response does not
+    if(error){
+        console.log('Unable to connect to weather service')
+    } else if (response.body.error) { // Fail at request input
+        console.log('Unable find location')
+    }
+    else {
+        console.log('It is currently ', response.body.currently.temperature)
+    }
 })
+
+const geocodeURL = 'yyyy'
+// Parse request as json
+request({ url: geocodeURL, json: true }, (error, response) => {
+    if(error){
+        console.log('Unable to connect to location services')
+    } else if (response.body.features.length === 0){
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+    
+        console.log(latitude, longitude)
+    }
+})
+
