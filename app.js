1b2948f2b16bb8556eb581c253bc41d5c6375e79
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-23 18:28:12 -0700

    Chaining async calls

--- app.js
+++ app.js
@@ -1,56 +1,21 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const url = 'xxxx'
-
-// Parse request as json
-request({ url: url, json: true }, (error, response) => {
-    // Fail at a low level; error argument exists, but response does not
-    if(error){
-        console.log('Unable to connect to weather service')
-    } else if (response.body.error) { // Fail at request input
-        console.log('Unable find location')
-    }
-    else {
-        console.log('It is currently ', response.body.currently.temperature)
-    }
-})
-
-const geocodeURL = 'yyyy'
-// Parse request as json
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if(error){
-        console.log('Unable to connect to location services')
-    } else if (response.body.features.length === 0){
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-    
-        console.log(latitude, longitude)
-    }
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
-
-
-
-
-
-
-
-
-
-
-
-
 geocode('New York', (error, data) => {
+    // If error stop function execution
+    if(error){
+        return console.log(error)
+    }
+
     console.log('Error ', error)
     console.log('Data ', data)
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error){
+            return error
+        }
+        
+        console.log('Error', error)
+        console.log('Data', data)
+    })
 })
-
-
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,9 +4,10 @@ const forecast = (lat, long, callback) => {
     const url = 'xxxx'
 
     request({ url: url, json: true }, (error, response) => {
+        // Fail at a low level; error argument exists, but response does not
         if(error){
             callback('Unable to connect to weather service', undefined)
-        } else if(response.body.error){
+        } else if(response.body.error){ // Fail at request input
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, response.body.daily.data[0].summary)
