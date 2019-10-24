48f2723db5efdd53c58423a8df10e110175ffaaa
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-23 18:38:05 -0700

    Take address from user input

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('New York', (error, data) => {
-    // If error stop function execution
-    if(error){
-        return console.log(error)
-    }
+// Get access to command line arguments using process instead of yargs
+const address = process.argv[2]
 
-    console.log('Error ', error)
-    console.log('Data ', data)
-
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if(!address){
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        // If error stop function execution
         if(error){
-            return error
+            return console.log(error)
         }
-        
-        console.log('Error', error)
-        console.log('Data', data)
+    
+        console.log('Error ', error)
+        console.log('Data ', data)
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return error
+            }
+    
+            console.log('Error', error)
+            console.log('Data', data)
+        })
     })
-})
+}
