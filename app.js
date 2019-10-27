9a0670f111417dc7207f3a6e5424d52e12353887
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-26 19:16:37 -0700

    Use destructuring and property shorthand

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ const address = process.argv[2]
 if(!address){
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location}) => {
         // If error stop function execution
         if(error){
             return console.log(error)
         }
-    
-        console.log('Error ', error)
-        console.log('Data ', data)
-    
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return error
             }
     
-            console.log('Error', error)
-            console.log('Data', data)
+            console.log(location)
+            console.log(forecastData)
         })
     })
 }
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 const forecast = (lat, long, callback) => {
     const url = 'xxxx'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         // Fail at a low level; error argument exists, but response does not
         if(error){
             callback('Unable to connect to weather service', undefined)
-        } else if(response.body.error){ // Fail at request input
+        } else if(body.error){ // Fail at request input
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary)
+            callback(undefined, body.daily.data[0].summary)
         }
     })
 
